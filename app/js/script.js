9124a0dd8210c3aa7854302772d8f8d5355804f5
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2022-12-20 20:55:09 +0200

    Add overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,19 +1,24 @@
 const body = document.querySelector('.body');
+const overlay = document.querySelector('.overlay');
 const menu = document.querySelector('.header__menu');
+const menuOptions = document.querySelectorAll('.header__menu-link');
 const openMenuBtn = document.querySelector('.header__hamburger');
 const closeMenuBtn = document.querySelector('.header__menu-btn');
-const menuOptions = document.querySelectorAll('.header__menu-link');
 
 const openMenu = () => {
   menu.classList.remove('close');
+  overlay.classList.remove('close');
   menu.classList.add('open');
+  overlay.classList.add('open');
   body.classList.add('noscroll');
 };
 
 const closeMenu = () => {
   menu.classList.remove('open');
-  menu.classList.add('close');
+  overlay.classList.remove('open');
   body.classList.remove('noscroll');
+  menu.classList.add('close');
+  overlay.classList.add('close');
 };
 
 openMenuBtn.addEventListener('click', () => {
