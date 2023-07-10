95d567ff9b77c45d8879f8f487584ae4c2b7c0f1
Miles Mosweu <tmtmosweu@gmail.com>
2023-07-10 20:39:53 +0200

    Split JS files into modules

--- modules/script.js
+++ modules/script.js
@@ -1,3 +1,5 @@
+import projectsArr from './projects.js';
+
 const body = document.querySelector('.body');
 
 const menu = document.querySelector('.header__menu');
@@ -46,69 +48,6 @@ const popupCancelBtn = document.querySelector('.project__details-cancel-icon');
 
 const popupTitle = document.querySelector('.project__feature-title');
 const popupDescrption = document.querySelector('.project__details-description');
-
-const projectsArr = [
-  {
-    ctaId: '1',
-    name: 'Awesome books',
-    description:
-      'Awesome Books is a dynamic single page web app that displays a list of books and allows you to add and remove books from that list. The books are managed and manipulated using classes and helper functions within those classes. The displayed books are stored and retrieved from the local storage of the browser.',
-    image: 'images/Awesome-books-popup.svg',
-    technologies: ['Github', 'JavaScript', 'Terminal', 'css', 'html', 'Luxon'],
-    source: 'https://github.com/Timbar09/Awesome-books-vol-2',
-    live: 'https://timbar09.github.io/Awesome-books-vol-2/',
-  },
-  {
-    ctaId: '2',
-    name: "Africa's Leading Tech Confrence 2023",
-    description:
-      "This project is a dynamic conference web page.The project was built with JavaScript, CSS and HTML. On the speakers section, speaker data is stored in browswer's local storage and the data is lretrieved when the page loads.",
-    image: 'images/ALT-Conference-popup.svg',
-    technologies: ['JavaScript', 'GitHub', 'css', 'Terminal', 'html'],
-    source: 'https://github.com/Timbar09/Africas-Leading-Tech-Conference',
-    live: 'https://timbar09.github.io/Africas-Leading-Tech-Conference/',
-  },
-  {
-    ctaId: '3',
-    name: 'Project 3 title',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam libero accusantium quibusdam quis, voluptatem rem deserunt, quod iste labore tenetur dolores officia natus eaque neque consequuntur necessitatibus nemo nostrum eos nulla quo dolore cumque dolor!',
-    image: 'images/project3-desktop.svg',
-    technologies: ['Github', 'Bootstrap', 'Terminal'],
-    source: '',
-    live: '',
-  },
-  {
-    ctaId: '4',
-    name: 'Project 4 title',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque reprehenderit possimus hic dolore unde fugit suscipit, voluptas dolorum nobis debitis.',
-    image: 'images/project1-desktop.svg',
-    technologies: ['Github', 'JavaScript', 'Bootstrap', 'Terminal'],
-    source: '',
-    live: '',
-  },
-  {
-    ctaId: '5',
-    name: 'Project 5 title',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non voluptas ut, maxime eos quos incidunt commodi aut libero explicabo enim laboriosam, similique quidem id ipsa. Harum reprehenderit iste ad officia beatae aperiam vel nisi nobis, dolorem nulla.',
-    image: 'images/project2-desktop.svg',
-    technologies: ['Codekit', 'JavaScript', 'Terminal', 'Codepen'],
-    source: '',
-    live: '',
-  },
-  {
-    ctaId: '6',
-    name: 'Project 6 title',
-    description:
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et, voluptatem aliquid. Omnis similique animi doloribus ipsam, incidunt excepturi explicabo esse voluptatum sit error, adipisci dolore ipsa praesentium beatae, fugit velit accusamus.',
-    image: 'images/project3-desktop.svg',
-    technologies: ['Codekit', 'Github', 'JavaScript', 'Bootstrap', 'Terminal'],
-    source: '',
-    live: '',
-  },
-];
 
 // Open popup
 const activatePopup = () => {
