8cd9e62e6da485ac0619c473d633c1df7d8566fd
Miles Mosweu <tmtmosweu@gmail.com>
2023-07-14 14:06:33 +0200

    Update projest data

--- modules/projects.js
+++ modules/projects.js
@@ -2,9 +2,15 @@ const projectsArr = [
   {
     ctaId: '1',
     name: 'Awesome books',
-    description:
-      'Awesome Books is a dynamic single page web app that displays a list of books and allows you to add and remove books from that list. The books are managed and manipulated using classes and helper functions within those classes. The displayed books are stored and retrieved from the local storage of the browser.',
-    image: 'images/Awesome-books-popup.svg',
+    description: [
+      'Awesome Books is a feature-rich single-page web application designed to provide an interactive and user-friendly platform for managing a list of books. <br /> With its dynamic functionality, users can effortlessly add and remove books from the list, creating a personalized library experience.',
+      'The core of Awesome Books revolves around the effective utilization of classes and helper functions. By employing object-oriented programming principles, the application leverages classes to encapsulate book-related data and behaviors. Each book is represented by an instance of a class, allowing for easy manipulation and organization of book information.',
+      'The application seamlessly integrates with the browser\'s local storage, providing persistent data storage for the book collection. This means that users can add and remove books without worrying about losing their progress or having to recreate their library each time they visit the website. The data is securely stored within the browser, ensuring a seamless and uninterrupted experience across sessions.',
+      'Through a clean and intuitive user interface, Awesome Books offers a smooth and engaging browsing experience. Users can quickly add new books to the list, specifying details such as the title and author. The application also allows for effortless removal of books, providing a streamlined way to manage the library based on individual preferences and reading habits.',
+      'Whether users are avid readers, book enthusiasts, or simply looking for an organized way to keep track of their favorite titles, Awesome Books caters to their needs. With its user-centric design and efficient implementation, the application aims to enhance the book management experience, making it more enjoyable and convenient.',
+      'Overall, Awesome Books represents a powerful yet user-friendly solution for book lovers, providing an efficient way to manage and organize a personalized book collection. With its dynamic features, seamless integration with local storage, and intuitive user interface, it sets out to redefine the way users interact with their books online.',
+    ],
+    image: 'images/awesome-books-popup.svg',
     technologies: ['Github', 'JavaScript', 'Terminal', 'css', 'html', 'Luxon'],
     source: 'https://github.com/Timbar09/Awesome-books-vol-2',
     live: 'https://timbar09.github.io/Awesome-books-vol-2/',
@@ -12,8 +18,15 @@ const projectsArr = [
   {
     ctaId: '2',
     name: "Africa's Leading Tech Confrence 2023",
-    description:
-      "This project is a dynamic conference web page.The project was built with JavaScript, CSS and HTML. On the speakers section, speaker data is stored in browswer's local storage and the data is lretrieved when the page loads.",
+    description: [
+      'This project is a responsive website consisting of multiple pages, including a home page and an about page. The website is designed and developed to provide an optimal viewing experience on both mobile and desktop devices, ensuring seamless navigation and functionality.',
+      'The home page serves as the central hub of the website, featuring a menu with a link to the about page. Users can easily navigate to the about page to access additional information about the project or organization.',
+      'In the header section of the website, the logo is strategically placed and links back to the home page, allowing users to quickly return to the main page regardless of their location within the website.',
+      'For mobile devices, a mobile menu is implemented. When users click or tap the hamburger button in the header, the mobile menu overlays the page, providing easy access to different sections and pages. The mobile menu is designed to be visually consistent with the overall website design, incorporating appropriate colors, typography, and spacing. To close the mobile menu, a close (X) button is provided, allowing users to seamlessly return to the main content.',
+      'A dynamic page feature is implemented, specifically in the \'Featured speakers\' section. The speaker data is stored in a JavaScript variable and utilized during the page load to dynamically generate the HTML content for this section. This dynamic approach allows for easy updating and modification of the featured speakers without directly modifying the HTML code.',
+      'The responsive design of the website ensures that it adapts to different screen sizes and devices. Whether accessed on a mobile device with a narrow viewport or a desktop computer, the website layout and content are optimized for the respective screen size, providing users with a visually appealing and user-friendly experience.',
+      'In summary, this project encompasses a responsive website with multiple pages, including a home page and an about page. It features a mobile menu for smaller screen sizes, dynamic content generation for the \'Featured speakers\' section, and a cohesive navigation system that allows users to seamlessly explore the website on both mobile and desktop devices.',
+    ],
     image: 'images/ALT-Conference-popup.svg',
     technologies: ['JavaScript', 'GitHub', 'css', 'Terminal', 'html'],
     source: 'https://github.com/Timbar09/Africas-Leading-Tech-Conference',
@@ -22,8 +35,9 @@ const projectsArr = [
   {
     ctaId: '3',
     name: 'Project 3 title',
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam libero accusantium quibusdam quis, voluptatem rem deserunt, quod iste labore tenetur dolores officia natus eaque neque consequuntur necessitatibus nemo nostrum eos nulla quo dolore cumque dolor!',
+    description: [
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque reprehenderit possimus hic dolore unde fugit suscipit, voluptas dolorum nobis debitis.',
+    ],
     image: 'images/project3-desktop.svg',
     technologies: ['Github', 'Bootstrap', 'Terminal'],
     source: '',
@@ -32,8 +46,9 @@ const projectsArr = [
   {
     ctaId: '4',
     name: 'Project 4 title',
-    description:
+    description: [
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque reprehenderit possimus hic dolore unde fugit suscipit, voluptas dolorum nobis debitis.',
+    ],
     image: 'images/project1-desktop.svg',
     technologies: ['Github', 'JavaScript', 'Bootstrap', 'Terminal'],
     source: '',
@@ -42,8 +57,9 @@ const projectsArr = [
   {
     ctaId: '5',
     name: 'Project 5 title',
-    description:
+    description: [
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non voluptas ut, maxime eos quos incidunt commodi aut libero explicabo enim laboriosam, similique quidem id ipsa. Harum reprehenderit iste ad officia beatae aperiam vel nisi nobis, dolorem nulla.',
+    ],
     image: 'images/project2-desktop.svg',
     technologies: ['Codekit', 'JavaScript', 'Terminal', 'Codepen'],
     source: '',
@@ -52,8 +68,9 @@ const projectsArr = [
   {
     ctaId: '6',
     name: 'Project 6 title',
-    description:
+    description: [
       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et, voluptatem aliquid. Omnis similique animi doloribus ipsam, incidunt excepturi explicabo esse voluptatum sit error, adipisci dolore ipsa praesentium beatae, fugit velit accusamus.',
+    ],
     image: 'images/project3-desktop.svg',
     technologies: ['Codekit', 'Github', 'JavaScript', 'Bootstrap', 'Terminal'],
     source: '',
