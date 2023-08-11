4a98b45cf7f77a5097c82cccefef16eb8ff14e85
Miles Mosweu <tmtmosweu@gmail.com>
2023-08-11 19:27:59 +0200

    Add projects

--- modules/projects.js
+++ modules/projects.js
@@ -4,10 +4,14 @@ const projectsArr = [
     name: 'Awesome books',
     description: [
       'Awesome Books is a feature-rich single-page web application designed to provide an interactive and user-friendly platform for managing a list of books. <br /> With its dynamic functionality, users can effortlessly add and remove books from the list, creating a personalized library experience.',
+
       'The core of Awesome Books revolves around the effective utilization of classes and helper functions. By employing object-oriented programming principles, the application leverages classes to encapsulate book-related data and behaviors. Each book is represented by an instance of a class, allowing for easy manipulation and organization of book information.',
+
       "The application seamlessly integrates with the browser's local storage, providing persistent data storage for the book collection. This means that users can add and remove books without worrying about losing their progress or having to recreate their library each time they visit the website. The data is securely stored within the browser, ensuring a seamless and uninterrupted experience across sessions.",
+
       'Through a clean and intuitive user interface, Awesome Books offers a smooth and engaging browsing experience. Users can quickly add new books to the list, specifying details such as the title and author. The application also allows for effortless removal of books, providing a streamlined way to manage the library based on individual preferences and reading habits.',
       'Whether users are avid readers, book enthusiasts, or simply looking for an organized way to keep track of their favorite titles, Awesome Books caters to their needs. With its user-centric design and efficient implementation, the application aims to enhance the book management experience, making it more enjoyable and convenient.',
+
       'Overall, Awesome Books represents a powerful yet user-friendly solution for book lovers, providing an efficient way to manage and organize a personalized book collection. With its dynamic features, seamless integration with local storage, and intuitive user interface, it sets out to redefine the way users interact with their books online.',
     ],
     image: 'images/projects/Awesome-books-popup.svg',
@@ -20,11 +24,17 @@ const projectsArr = [
     name: "Africa's Leading Tech Confrence 2023",
     description: [
       'This project is a responsive website consisting of multiple pages, including a home page and an about page. The website is designed and developed to provide an optimal viewing experience on both mobile and desktop devices, ensuring seamless navigation and functionality.',
+
       'The home page serves as the central hub of the website, featuring a menu with a link to the about page. Users can easily navigate to the about page to access additional information about the project or organization.',
+
       'In the header section of the website, the logo is strategically placed and links back to the home page, allowing users to quickly return to the main page regardless of their location within the website.',
+
       'For mobile devices, a mobile menu is implemented. When users click or tap the hamburger button in the header, the mobile menu overlays the page, providing easy access to different sections and pages. The mobile menu is designed to be visually consistent with the overall website design, incorporating appropriate colors, typography, and spacing. To close the mobile menu, a close (X) button is provided, allowing users to seamlessly return to the main content.',
+
       "A dynamic page feature is implemented, specifically in the 'Featured speakers' section. The speaker data is stored in a JavaScript variable and utilized during the page load to dynamically generate the HTML content for this section. This dynamic approach allows for easy updating and modification of the featured speakers without directly modifying the HTML code.",
+
       'The responsive design of the website ensures that it adapts to different screen sizes and devices. Whether accessed on a mobile device with a narrow viewport or a desktop computer, the website layout and content are optimized for the respective screen size, providing users with a visually appealing and user-friendly experience.',
+
       "In summary, this project encompasses a responsive website with multiple pages, including a home page and an about page. It features a mobile menu for smaller screen sizes, dynamic content generation for the 'Featured speakers' section, and a cohesive navigation system that allows users to seamlessly explore the website on both mobile and desktop devices.",
     ],
     image: 'images/projects/ALT-Conference-popup.svg',
@@ -38,8 +48,10 @@ const projectsArr = [
     description: [
       'This project is a to-do list web app that allows users to manage their tasks effectively. It provides a user-friendly interface to display a list of tasks and enables users to add new tasks and remove existing ones.',
       '',
+
       'The project was developed using Webpack, JavaScript, CSS, and HTML. Webpack is used as the module bundler to manage dependencies and optimize the build process. JavaScript is used to implement the interactive functionality of the to-do list, such as adding and removing tasks dynamically. CSS is used for styling the user interface and providing a visually appealing design. HTML is used for the structure and layout of the web app.',
       '',
+
       'Overall, this project showcases the utilization of essential web development technologies and techniques to create a functional and user-friendly to-do list web app.',
     ],
     image: 'images/projects/todo_app_detail.svg',
@@ -56,36 +68,74 @@ const projectsArr = [
   },
   {
     ctaId: '4',
-    name: 'Project 4 title',
+    name: 'BudgetTrail',
     description: [
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque reprehenderit possimus hic dolore unde fugit suscipit, voluptas dolorum nobis debitis.',
+      'This handy mobile web app is carefully designed for users to track and optimize their finances. The app allows users to take control of their budget and manage their income and expenses in a comprehensive way.',
+
+      "It is built on a robust architecture that combines the power of Ruby on Rails, PostgreSQL, and SASS to ensure a seamless and responsive user experience. This synergy not only guarantees the application's reliability, but also enables it to efficiently process and store data, safeguarding users' financial information.",
+
+      'Navigating through this user-friendly platform, individuals can effortlessly input and monitor their day-to-day transactions, assigning them to specific categories that align with their personal financial schema. By categorizing transactions, users gain unparalleled clarity into their spending patterns, unveiling insights that are instrumental in making informed financial decisions.',
+
+      "The backbone of this application's user experience is fortified by Devise, an authentication solution that guarantees not only the security of user data but also a streamlined and hassle-free login process. Our implementation of Rspec-rails and Capybara further ensures that the application's functionalities are rigorously tested, guaranteeing its reliability and minimizing potential disruptions.",
+
+      "With this mobile web application, you can turn financial prudence into a reachable goal. Explore the capabilities of comprehensive financial management, organized categorization, and insights driven by data. All these are wrapped in an interface that focuses on user experience while ensuring security and functionality aren't compromised. Start stepping towards the future of budget management today.",
     ],
-    image: 'images/projects/project1-desktop.svg',
-    technologies: ['Github', 'JavaScript', 'Bootstrap', 'Terminal'],
-    source: '',
-    live: '',
+    image: 'images/projects/budgettrail_popup.svg',
+    technologies: [
+      'Ruby on Rails',
+      'PostgreSQL',
+      'SASS',
+      'Rspec-rails',
+      'Devise',
+      'Capybara',
+      'Rubocop',
+      'Github',
+      'Terminal',
+    ],
+    source: 'https://github.com/Timbar09/BudgetTrail',
+    live: 'https://budgettrail.onrender.com/',
   },
   {
     ctaId: '5',
-    name: 'Project 5 title',
+    name: "Space Traveler's Hub",
     description: [
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Non voluptas ut, maxime eos quos incidunt commodi aut libero explicabo enim laboriosam, similique quidem id ipsa. Harum reprehenderit iste ad officia beatae aperiam vel nisi nobis, dolorem nulla.',
     ],
-    image: 'images/projects/project2-desktop.svg',
-    technologies: ['Codekit', 'JavaScript', 'Terminal', 'Codepen'],
-    source: '',
-    live: '',
+    image: 'images/projects/space_popup.svg',
+    technologies: ['React.js', 'Redux-toolkit', 'Bootstrap', 'REST API'],
+    source:
+      'https://github.com/Timbar09/React-Redux-Group-Project-Space-Travelers-Hub',
+    live: 'https://spacetravelershub-react.onrender.com/',
   },
   {
     ctaId: '6',
-    name: 'Project 6 title',
+    name: 'Blogister',
     description: [
-      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et, voluptatem aliquid. Omnis similique animi doloribus ipsam, incidunt excepturi explicabo esse voluptatum sit error, adipisci dolore ipsa praesentium beatae, fugit velit accusamus.',
+      "This seamlessly operational web application serves as an interactive canvas, presenting a collection of posts that allows users to engage actively. Within this app, users can actively participate in conversations  by appending comments and expressing appreciation through likes. Designed to cater to bloggers' goals, this platform provides a way to share thoughts, concepts, and personal experiences with an interested audience.",
+
+      'The foundation of this dynamic experience is built using a combination of technologies, including Ruby on Rails, PostgreSQL, and SASS. These components work together to create a seamless, responsive, and secure application environment, allowing users to explore and contribute with confidence.',
+
+      'In this user-friendly environment, users encounter a carefully selected collection of posts, encouraging them to explore stories that align with their interests. This platform promotes active engagement, an embodiment of the contemporary digital experience where opinions and reactions play a role in shaping the narrative.',
+
+      'Powered by the versatility of Devise, a trusted authentication solution, the application ensures user data remains shielded, while the confluence of Rspec-rails and Capybara guarantees a meticulous validation of its functionalities, minimizing any potential disruptions.',
+
+      'In essence, what emerges is an immersive web application that not only disseminates information but also fosters a thriving community of discourse. This platform offers bloggers an avenue to broadcast their insights, opinions, and stories to an enthusiastic audience. Amidst the world of technology and communication, this application serves as a reminder of the impact of connectivity and the art of digital expression.',
     ],
-    image: 'images/projects/project3-desktop.svg',
-    technologies: ['Codekit', 'Github', 'JavaScript', 'Bootstrap', 'Terminal'],
-    source: '',
-    live: '',
+    image: 'images/projects/blog_popup.svg',
+    technologies: [
+      'Ruby on Rails',
+      'PostgreSQL',
+      'SASS',
+      'Rspec-rails',
+      'Devise',
+      'CanCanCan',
+      'Capybara',
+      'Rubocop',
+      'Github',
+      'Terminal',
+    ],
+    source: 'https://github.com/Timbar09/Blogister',
+    live: '#',
   },
 ];
 
