3b41d5e11a401912262f8fe1d344efab7e73166a
Miles Mosweu <tmtmosweu@gmail.com>
2023-09-04 21:59:52 +0200

    Resolve linters

--- modules/functions/darkmode.js
+++ modules/functions/darkmode.js
@@ -1,8 +1,7 @@
 const enableDarkMode = () => {
   document.querySelector('body').classList.add('darkmode');
   document.querySelector('.header__toggle').title = 'Switch to light mode';
-  document.querySelector('.header__toggle span').innerHTML =
-    '<i class="fa-regular fa-moon"></i>';
+  document.querySelector('.header__toggle span').innerHTML = '<i class="fa-regular fa-moon"></i>';
 
   localStorage.setItem('darkMode', 'enabled');
 };
@@ -10,8 +9,7 @@ const enableDarkMode = () => {
 const disableDarkMode = () => {
   document.querySelector('body').classList.remove('darkmode');
   document.querySelector('.header__toggle').title = 'Switch to dark mode';
-  document.querySelector('.header__toggle span').innerHTML =
-    '<i class="fa-regular fa-sun"></i>';
+  document.querySelector('.header__toggle span').innerHTML = '<i class="fa-regular fa-sun"></i>';
 
   localStorage.setItem('darkMode', null);
 };
