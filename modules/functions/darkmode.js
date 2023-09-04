4d1f621e757700638a649d9f80b44e0299708815
Miles Mosweu <tmtmosweu@gmail.com>
2023-09-04 12:44:53 +0200

    Add darkmode toggle functionality

---
+++ modules/functions/darkmode.js
@@ -0,0 +1,19 @@
+const enableDarkMode = () => {
+  document.querySelector('body').classList.add('darkmode');
+  document.querySelector('.header__toggle').title = 'Switch to light mode';
+  document.querySelector('.header__toggle span').innerHTML =
+    '<i class="fa-regular fa-moon"></i>';
+
+  localStorage.setItem('darkMode', 'enabled');
+};
+
+const disableDarkMode = () => {
+  document.querySelector('body').classList.remove('darkmode');
+  document.querySelector('.header__toggle').title = 'Switch to dark mode';
+  document.querySelector('.header__toggle span').innerHTML =
+    '<i class="fa-regular fa-sun"></i>';
+
+  localStorage.setItem('darkMode', null);
+};
+
+export { enableDarkMode, disableDarkMode };
