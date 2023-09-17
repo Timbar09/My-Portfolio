e13e3e9e21fa66abe69ff8783932524577382bce
Miles Mosweu <tmtmosweu@gmail.com>
2023-09-17 11:26:03 +0200

    Setup server

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,17 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name][contenthash].js',
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    port: 3000,
+    hot: true,
+    open: true,
+    compress: true,
+    historyApiFallback: true,
   },
   module: {
     rules: [
